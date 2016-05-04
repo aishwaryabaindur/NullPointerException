d0e4b72f015c4fd11e7adc1866024c7ac926349f
tilakraj <natue7595@gmail.com>
2016-05-04 06:38:06 -0700

    Threads

---
+++ Threads.java
@@ -0,0 +1,32 @@
+ 
+class MyRunnable implements Runnable {
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread a = new Thread(r);
+		Thread b = new Thread(r);
+		Thread c = new Thread(r);
+		a.setName("thread a ");
+		b.setName("thread b ");
+		c.setName("thread c ");
+		a.setPriority(Thread.MIN_PRIORITY);
+		b.setPriority(Thread.NORM_PRIORITY);
+		c.setPriority(Thread.MAX_PRIORITY);
+		a.start();
+		b.start();
+		c.start();
+	}
+	
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+		System.out.println(
+			Thread.currentThread().getName() 
+				+ i + " is running");
+			try {
+				Thread.sleep(2000);
+			} 
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}	
+}
